cb70d2d816b720742a5d4e83240d2143fe34279e
wise-Kaas <kamilaliev@hotmail.com>
2021-03-05 11:38:46 +0300

    [vagrant] epam lab turned on ssl

--- vagrant/epam_lab/provisioners/install_nginx.sh
+++ vagrant/epam_lab/provisioners/install_nginx.sh
@@ -8,4 +8,8 @@ sudo mv /home/vagrant/wordpress /etc/nginx/sites-available/wordpress
 sudo ln -s /etc/nginx/sites-available/wordpress /etc/nginx/sites-enabled/wordpress
 sudo rm /etc/nginx/sites-enabled/default
 
+sudo openssl req -x509 -days 365 -config /vagrant/files/openssl.cfg -out /etc/ssl/certs/nginx-selfsigned.crt
+sudo mv nginx-selfsigned.key /etc/ssl/private
+sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+
 sudo service nginx restart

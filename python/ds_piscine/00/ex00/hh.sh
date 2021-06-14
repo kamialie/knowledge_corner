d18b295c3146a65632a8a902e96fa82dc4a30fe0
wise-Kaas <kamilaliev@hotmail.com>
2021-06-14 12:04:09 +0300

    [python] [ds piscine] added ex00

---
+++ python/ds_piscine/00/ex00/hh.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# check if jq is installed
+if ! command -v jq > /dev/null 2>&1; then
+	echo "please install jq tool"
+	exit 1
+fi
+
+read -p "Please, specify the  name of a vacancy: " vacancy
+
+BASE_URL="https://api.hh.ru"
+
+PAGE=0
+PER_PAGE=20
+
+curl -X GET ${BASE_URL}/vacancies \
+	--data-urlencode "page=${PAGE}" \
+	--data-urlencode "per_page=${PER_PAGE}" \
+	--data-urlencode "text=${vacancy}" | jq > hh.json
